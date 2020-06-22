7b4233e39c3e47eb1e61f853520fdc6f4960232d
yelang <125223892@qq.com>
2020-06-22 17:22:22 +0800

    Add visitor page content

--- src/components/page/visitor.vue
+++ src/components/page/visitor.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
         <head-top></head-top>
-        用户分布
+        <visitor-pie :pieData = "pieData" ></visitor-pie>
     </div>
 </template>
 
 
 <script>
 import headTop from '../headTop'
+import visitorPie from '../visitorPie'
+
 export default {
+    data(){
+        return {
+            pieData: {
+                beijing: 100,
+                shanghai: 200,
+                shenzhen: 300,
+                hangzhou: 200,
+                qita: 700
+            }
+        }
+    },
     components: {
-        headTop
+        headTop,
+        visitorPie
     }
 }
 </script>
+
+
+<style lang="less">
+	@import '../../style/mixin.less';
+	
+</style>
