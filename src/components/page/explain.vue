66e5fedfa3ed88e99bfcb957b2621c79ec50c02a
yelang <125223892@qq.com>
2020-06-24 11:12:19 +0800

    Add explain page content

--- src/components/page/explain.vue
+++ src/components/page/explain.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <head-top></head-top>
-        说明
+        <p class = "explain_text">node-elm 后台管理系统</p>
+        <p class = "explain_text">第一次登录的用户自动注册成为普通管理员</p>
+        <p class = "explain_text">普通管理员可以添加，修改信息</p>
+        <p class = "explain_text">超级管理员可以删除信息</p>
     </div>
 </template>
 
@@ -14,3 +17,13 @@ export default {
     }
 }
 </script>
+
+<style lang="less" scoped>
+    @import '../../style/mixin.less';
+    .explain_text{
+        margin-top: 20px;
+        text-align: center;
+        font-size: 20px;
+        color: #333
+    }
+</style>
