7d8be283ff84647694c1869db285a44bab549e15
yelang <125223892@qq.com>
2020-06-24 10:57:24 +0800

    Add adminSet page content

--- src/components/page/adminSet.vue
+++ src/components/page/adminSet.vue
@@ -1,7 +1,29 @@
 <template>
     <div>
         <head-top></head-top>
-        管理员设置
+        <header class = "admin_title">管理员信息</header>
+        <div class = "admin_set">
+            <ul>
+                <li>
+                    <span>姓名：</span><span>{{adminInfo.user_name}}</span>
+                </li>
+                <li>
+                    <span>注册时间：</span><span>{{adminInfo.create_time}}</span>
+                </li>
+                <li>
+                    <span>管理员权限：</span><span>{{adminInfo.admin}}</span>
+                </li>
+                <li>
+                    <span>管理员ID：</span><span>{{adminInfo.id}}</span>
+                </li>
+                <li>
+                    <span>更换头像：</span>
+                    <!-- <el-upload>
+
+                    </el-upload> -->
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -9,8 +31,37 @@
 <script>
 import headTop from '../headTop'
 export default {
+    data(){
+        return {
+            adminInfo: {
+
+            }
+        }
+    },
     components: {
         headTop
     }
 }
 </script>
+<style lang="less" scoped>
+    @import '../../style/mixin.less';
+    .admin_set{
+        width: 60%;
+        background-color: #F9FAFC;
+        min-height: 400px;
+        margin: 20px auto 0;
+        border-radius: 10px;
+        ul > li {
+            padding: 20px;
+            span{
+                color: #666;
+            }
+        }
+    }
+
+    .admin_title{
+        margin-top: 20px;
+        .sc(24px, #666);
+        text-align: center;
+    }
+</style>
--- src/components/page/vueEdit.vue
+++ src/components/page/vueEdit.vue
@@ -1,16 +1,63 @@
 <template>
     <div>
         <head-top></head-top>
-        文本编辑
+        <div class = "edit_container">
+            <quill-editor 
+                v-model = "content"
+                ref = "myQuillEditor"
+                class = "editer"
+                :options = "editorOption"
+                @ready = "onEditorReady($event)">
+            </quill-editor>
+        </div>
+        <div class = "submit_btn">
+            <el-button type = "primary" @click = "submit">提交</el-button>
+        </div>
     </div>
 </template>
 
 
 <script>
 import headTop from '../headTop'
+import {quillEditor} from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 export default {
+    data(){
+        return {
+            content: '<h3>文本编辑</h3>',
+            editorOption: {
+
+            }
+        }
+    },
+    methods: {
+        onEditorReady(editor){
+            console.log('editor ready!', editor)
+        },
+        submit(){
+            console.log(this.content);
+            this.$message.success('提交成功!');
+        }
+    },
     components: {
-        headTop
+        headTop,
+        quillEditor
     }
 }
 </script>
+
+<style lang="less" scoped>
+    @import '../../style/mixin.less';
+    .edit_container{
+        padding: 40px;
+        margin-bottom: 40px;
+    }
+    .editer{
+        height: 350px;
+    }
+    .submit_btn{
+        text-align: center;
+    }
+</style>
